ce5723a4225e7c9b45528d6c74acf8a128d814d8
Chloe Everson <chloeliz93@gmail.com>
2021-03-27 18:49:52 -0500

    tested addToCollection function by calling function while adding input to function - making album and logging new album to console. Then console logged collection.

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -9,6 +9,15 @@ function addToCollection(title, artist, yearPublished){
     yearPublished: yearPublished
   }
   collection.push(album);
-  return album 
+  return album
 }
-//
+//created addToCollection function which makes album from items called then adds to collection array then returns the album created.
+console.log(addToCollection('Blonde', 'Frank Ocean' , 2016));
+console.log(addToCollection('Amygdala', 'DJ Koze' , 2013));
+console.log(addToCollection("Summer's Gone", 'Odesza' , 2012));
+console.log(addToCollection('In Return', 'Odesza' , 2014));
+console.log(addToCollection('Teens Of Denial', 'Car Seat Headrest' , 2016));
+console.log(addToCollection('Love What Survives', 'Mount Kimbie' , 2017));
+//called function - made albums and returned them to Console
+console.log('album collection:' , collection);
+//logged collection to console 
