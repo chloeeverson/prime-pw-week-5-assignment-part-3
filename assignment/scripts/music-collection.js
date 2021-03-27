695038395b10f47d0cab394deb0f5ca7d5e882ce
Chloe Everson <chloeliz93@gmail.com>
2021-03-27 18:14:30 -0500

    created addToCollection function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,1 +1,14 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+//created new variable collection and set it to empty array
+function addToCollection(title, artist, yearPublished){
+  let album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+  }
+  collection.push(album);
+  return album 
+}
+//
